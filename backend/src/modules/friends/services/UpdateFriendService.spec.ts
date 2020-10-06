f7dc8f771cb683162abc725b4883a5ce80e96ed6
Luan Fernandes <luan.fj01@gmail.com>
2020-10-05 22:49:34 -0300

    test: should not be able to update an inexistent profile

---
+++ backend/src/modules/friends/services/UpdateFriendService.spec.ts
@@ -0,0 +1,42 @@
+import { ObjectID } from 'mongodb'
+
+import AppError from '@shared/errors/AppError'
+import FakeFriendsRepository from '../repositories/fakes/FakeFriendsRepository'
+import UpdateFriendService from './UpdateFriendService'
+
+let updateFriendProfile: UpdateFriendService
+let fakeFriendsRepository: FakeFriendsRepository
+
+describe('CreateFriend', () => {
+  beforeEach(() => {
+    fakeFriendsRepository = new FakeFriendsRepository()
+
+    updateFriendProfile = new UpdateFriendService(fakeFriendsRepository)
+  })
+
+  it('should be able to update a friend', async () => {
+    const friend = await fakeFriendsRepository.create({
+      name: 'John Doe',
+      email: 'any@email.com'
+    })
+
+    const updatedFriend = await updateFriendProfile.execute({
+      friend_id: friend.id,
+      name: 'John',
+      email: 'any@test.com'
+    })
+
+    expect(updatedFriend.name).toBe('John')
+    expect(updatedFriend.email).toBe('any@test.com')
+  })
+
+  it('should not be able to update an inexistent profile', async () => {
+    expect(
+      updateFriendProfile.execute({
+        friend_id: new ObjectID(),
+        name: 'Test',
+        email: 'any@email.com'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
